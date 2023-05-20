03cf2c11e5b326afac755ff1aee0a053efb5b9fb
longmaker2 <l.deng@alustudent.com>
2023-05-20 09:53:39 +0200

    added the required file

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# minimum operations
+import math
+
+
+def minOperations(n):
+    # minimum operations to get n to 1
+    if n <= 1:
+        return 0
+
+    operations = 0
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
+        while n % i == 0:
+            operations += i
+            n //= i
+
+    if n > 1:
+        operations += n
+
+    return operations
---
+++ minimum_operations/README.md
@@ -0,0 +1,4 @@
+# Minimum Operations
+- Prototype: def minOperations(n)
+- Returns an integer
+- If n is impossible to achieve, return 0
