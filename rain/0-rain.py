c1b1df24608755b08e6c94096a56f15bf4d11744
longmaker2 <l.deng@alustudent.com>
2023-06-05 02:50:33 +0200

    completed the task

---
+++ rain/0-rain.py
@@ -0,0 +1,21 @@
+def rain(walls):
+    if not walls:
+        return 0
+
+    total_water = 0
+    left_max = [0] * len(walls)
+    right_max = [0] * len(walls)
+
+    left_max[0] = walls[0]
+    for i in range(1, len(walls)):
+        left_max[i] = max(left_max[i-1], walls[i])
+
+    right_max[-1] = walls[-1]
+    for i in range(len(walls)-2, -1, -1):
+        right_max[i] = max(right_max[i+1], walls[i])
+
+    for i in range(len(walls)):
+        water_height = min(left_max[i], right_max[i]) - walls[i]
+        total_water += water_height
+
+    return total_water
---
+++ rain/README.md
@@ -0,0 +1,5 @@
+# Rain Water
+
+This program calculates the amount of rainwater that can be retained after it rains, given a relief map represented by a list of non-negative integers representing the heights of walls with a unit width of 1.
+
+## Author: Long Maker Long Deng <l.deng@alustudent.com>
